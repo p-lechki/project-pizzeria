85fc4e96915c1cad0bdaf1106bfe0a17bc262fa5
Paweł Lechki <p.lechki@me.com>
2019-10-23 21:56:23 +0200

    TASK 9.3

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,11 +1,41 @@
+import {
+  templates,
+  select
+} from '../settings.js';
+import {
+  utils
+} from '../utils.js';
+import {
+  AmountWidget
+} from './AmountWidget.js';
+
 export class Booking {
-  // constructor (element){
+  constructor(element) {
+    const thisBooking = this;
 
-  // }
-  // reneder(){
+    thisBooking.element = element;
+    thisBooking.reneder(thisBooking.element);
+    thisBooking.initWidgets();
+  }
+  reneder(element) {
+    const thisBooking = this;
 
-  // }
-  // initBooking(){
+    thisBooking.dom = {
+      wrapper: element,
+    };
 
-  // }
+    const generateHTML = templates.bookingWidget(thisBooking.dom.wrapper);
+    const visualElement = utils.createDOMFromHTML(generateHTML);
+    element.appendChild(visualElement);
+
+    thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = element.querySelector(select.booking.peopleAmount);
+  }
+
+  initWidgets() {
+    const thisBooking = this;
+
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+  }
 }
