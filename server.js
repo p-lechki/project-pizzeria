d2fee16c5006138aa1a147c22015b220e3dc6d38
Paweł Lechki <p.lechki@me.com>
2019-11-16 20:18:53 +0100

    10.4 Heroku issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('dist/db/app.js');
+const router = jsonServer.router('dist/db/app.json');
 const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true
@@ -11,4 +11,5 @@ const port = process.env.PORT || 3131;
 
 server.use(middlewares);
 server.use(router);
-server.use(port);
+
+server.listen(port);
